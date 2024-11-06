c7964408d4c87c7a92de98f74edb49ce5fb4637f
davidcarrillobyui <vrunitybro@gmail.com>
2024-11-05 21:55:16 -0700

    array, for and is statement created

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -11,6 +11,30 @@ public static class ArraySelector
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
+
+        //Arrray to store the final result
+        var result = new int[select.Length];
+
+        //Start both counters at 0
+        var l1Idx = 0;
+        var l2Idx =0;
+
+        //Go through each number in the select array
+        for(var i =0; i<select,Length; i++)
+        {
+            //if it is a 1 from list 1
+            if (select[i] ==1)
+            {
+                result[i] =list[l1Idx];
+                l1Idx++: // move to the bext number in the list1
+            }
+            else //if it is a 2 from list 2
+            {
+                result[i]=list2[l2Idx];
+                l2Idx++; //move tot he next number in list2
+            }
+        }
+
         return [];
     }
 }
--- week01/teach/Program.cs
+++ week01/teach/Program.cs
@@ -4,15 +4,15 @@ Divisors.Run();
 Console.WriteLine("\n======================\nArray Selector\n======================");
 ArraySelector.Run();
 
-// Uncomment when you are ready to run this
-// Console.WriteLine("\n======================\nAlgorithms\n======================");
-// Algorithms.Run();
+//  Uncomment when you are ready to run this
+ Console.WriteLine("\n======================\nAlgorithms\n======================");
+Algorithms.Run();
 
 
 
-// Uncomment and run as you get to the solution part
-// Console.WriteLine("\n======================\nDivisors Solution\n======================");
-// DivisorsSolution.Run();
-//
-// Console.WriteLine("\n======================\nArray Selector Solution\n======================");
-// ArraySelectorSolution.Run();
+//  Uncomment and run as you get to the solution part
+ Console.WriteLine("\n======================\nDivisors Solution\n======================");
+ DivisorsSolution.Run();
+
+ Console.WriteLine("\n======================\nArray Selector Solution\n======================");
+ ArraySelectorSolution.Run();
