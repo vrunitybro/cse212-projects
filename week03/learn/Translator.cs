b80a00c175f11ba2688718603533ac9849e94029
davidcarrillobyui <vrunitybro@gmail.com>
2024-11-20 01:16:55 -0700

    Translator

--- week03/learn/Translator.cs
+++ week03/learn/Translator.cs
@@ -6,25 +6,23 @@ public class Translator
         englishToGerman.AddWord("House", "Haus");
         englishToGerman.AddWord("Car", "Auto");
         englishToGerman.AddWord("Plane", "Flugzeug");
-        Console.WriteLine(englishToGerman.Translate("Car")); // Auto
-        Console.WriteLine(englishToGerman.Translate("Plane")); // Flugzeug
-        Console.WriteLine(englishToGerman.Translate("Train")); // ???
+
+        Console.WriteLine(englishToGerman.Translate("Car"));   // Output: Auto
+        Console.WriteLine(englishToGerman.Translate("Plane")); // Output: Flugzeug
+        Console.WriteLine(englishToGerman.Translate("Train")); // Output: ???
     }
 
     private Dictionary<string, string> _words = new();
 
     /// <summary>
-    /// Add the translation from 'from_word' to 'to_word'
-    /// For example, in a english to german dictionary:
-    /// 
-    /// my_translator.AddWord("book","buch")
+    /// Add the translation from 'fromWord' to 'toWord'.
+    /// For example, in an English to German dictionary:
+    /// myTranslator.AddWord("book", "buch").
     /// </summary>
-    /// <param name="fromWord">The word to translate from</param>
-    /// <param name="toWord">The word to translate to</param>
-    /// <returns>fixed array of divisors</returns>
+    /// <param name="fromWord">The word to translate from.</param>
+    /// <param name="toWord">The word to translate to.</param>
     public void AddWord(string fromWord, string toWord)
     {
-        // ADD YOUR CODE HERE
         // If the word already exists, update its translation.
         if (_words.ContainsKey(fromWord))
         {
@@ -37,10 +35,10 @@ public class Translator
     }
 
     /// <summary>
-    /// Translates the from word into the word that this stores as the translation
+    /// Translates the 'fromWord' into the word that this stores as the translation.
     /// </summary>
-    /// <param name="fromWord">The word to translate</param>
-    /// <returns>The translated word or "???" if no translation is available</returns>
+    /// <param name="fromWord">The word to translate.</param>
+    /// <returns>The translated word or "???" if no translation is available.</returns>
     public string Translate(string fromWord)
     {
         // Check if the word exists in the dictionary and return the translation.
