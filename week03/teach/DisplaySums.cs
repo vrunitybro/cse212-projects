f6178341c4a24629ceec5b33fbd101098d39ebc9
davidcarrillobyui <vrunitybro@gmail.com>
2024-11-21 09:58:38 -0700

    tested

--- week03/teach/DisplaySums.cs
+++ week03/teach/DisplaySums.cs
@@ -1,4 +1,8 @@
-﻿public static class DisplaySums {
+﻿using System;
+using System.Collections.Generic;
+
+public static class DisplaySums
+{
     public static void Run() {
         DisplaySumPairs([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
         // Should show something like (order does not matter):
@@ -23,11 +27,21 @@
 
     /// <summary>
     /// Display pairs of numbers (no duplicates should be displayed) that sum to
-    /// 10 using a set in O(n) time.  We are assuming that there are no duplicates
+    /// 10 using a set in O(n) time. We are assuming that there are no duplicates
     /// in the list.
     /// </summary>
     /// <param name="numbers">array of integers</param>
-    private static void DisplaySumPairs(int[] numbers) {
-        // TODO Problem 2 - This should print pairs of numbers in the given array
+    private static void DisplaySumPairs(int[] numbers)
+    {
+        var seen = new HashSet<int>();
+        foreach (var num in numbers)
+        {
+            var complement = 10 - num;
+            if (seen.Contains(complement))
+            {
+                Console.WriteLine($"{num} {complement}");
+            }
+            seen.Add(num);
+        }
     }
 }
