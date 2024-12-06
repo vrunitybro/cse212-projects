23f1aec41a3f70e448e92cf69f756043edd65319
vrunitybro <vrunitybro@gmail.com>
2024-12-06 01:22:02 -0700

    test

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -15,7 +15,7 @@ public static class Recursion
     public static int SumSquaresRecursive(int n)
     {
         if (n <= 0) return 0;
-        return n * n + SumSquaresRecursive(n - 1);;
+        return n * n + SumSquaresRecursive(n - 1);;;
     }
 
     /// <summary>
